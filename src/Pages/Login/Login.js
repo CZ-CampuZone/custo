560bc01a14ba81861a25f11aeaf6673b5e0d638d
sundaresan25 <sundarbgm98@gmail.com>
2022-05-06 16:14:54 +0530

    "Login"

--- src/Pages/Login/Login.js
+++ src/Pages/Login/Login.js
@@ -31,9 +31,10 @@ const useStyles = makeStyles({
   
   Bgicon4: {
     position: "fixed",
-    right: "37%",
-    bottom: "3%",
+    right: "33%",
+    bottom: "6%",
     zoom: "0.6",
+    transform: "rotate(204deg)",
   },
   Bgicon5: {
     position: "fixed", 
@@ -192,10 +193,17 @@ const Login = (props) => {
             <div className="shadow">
               <div class="modal-dialog modal-login">
                 <div class="modal-content">
-                
                   <div class="modal-header">
-                  
-                  <h4 class="">Hello Buddy!</h4>
+                    <h4 class="modal-title">Hello Buddy!</h4>
+                    {/* <button
+                      type="button"
+                      class="close"
+                      data-dismiss="modal"
+                      aria-hidden="true"
+                      onClick={() => { setModalstate(false) }}
+                    >
+                      &times;
+                    </button> */}
                   </div>
                   <div class="modal-body">
                     <form
@@ -234,13 +242,22 @@ const Login = (props) => {
                           {error}
                         </small>
                       )}
-                      <div class="form-group">
-                        <button onClick={forgotPassword ? resetHanler : handleLogin}
+                      <div class="form-group text-center">
+                      <button
+                         onClick={forgotPassword ? resetHanler : handleLogin}
                           type="submit"
-                          class="btn btn-primary btn-lg btn-block login-btn"
+                          style={{
+                            
+                            fontSize:"20px",
+                            color: "#fff",
+                            borderRadius: "20px",
+                            boxShadow: "0 3px 6px #00000036",
+                          }}
+                          class="btn mt-3 px-5 btntclr "
                         >
-                          {forgotPassword ? "Sent" : "Login"}
+                           {forgotPassword ? "Sent" : "Login"}
                         </button>
+                       
                       </div>
                     </form>
                   </div>
