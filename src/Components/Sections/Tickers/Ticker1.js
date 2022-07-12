78bd55a567c3cb13b234b67ab8acb87b054bbb8f
Martin Deva <martindevaprasad46@gmail.com>
2022-07-12 12:02:54 +0530

    newsupdate

--- src/Components/Sections/Tickers/Ticker1.js
+++ src/Components/Sections/Tickers/Ticker1.js
@@ -6,6 +6,7 @@ import { createTheme, ThemeProvider } from "@mui/material/styles";
 import React, { useContext, useEffect, useState, useRef } from "react";
 import AuthContext from "../../../Context/Context";
 import AssignmentTurnedInIcon from "@mui/icons-material/AssignmentTurnedIn";
+import Loader from "../../../loader/Loader";
 import {
   Avatar,
   List,
@@ -39,13 +40,16 @@ const useStyles = makeStyles((theme) => ({
   tickerBox: {
     boxShadow: "rgba(100, 100, 111, 0.2) 0px 7px 29px 0px",
     backgroundImage:
-      "linear-gradient( to left top, #9600c2, #b500b2, #cd00a2, #de0093, #eb1285 )",
+    "linear-gradient(to right top, #443899, #643393, #7d2d8b, #902882, #a02577)",
     borderBottomLeftRadius: "5px",
     borderTopLeftRadius: "5px",
   },
   btndec: {
     border: "1px solid #6E3666",
   },
+  iconAd:{
+    marginTop:"10px"
+  }
 }));
 
 export const Ticker1 = (props) => {
@@ -60,94 +64,48 @@ export const Ticker1 = (props) => {
       id: 1,
     },
   ];
-
-  //   React.useEffect(() => {
-  //     dispatch(GetNews({ admin: user?.username }));
-  //   }, [dispatch, newsCollection]);
-
-  //   const [newsForm, setNewsForm] = React.useState(false);
-  //   const [news, setNews] = React.useState("");
-  //   const [newsDate, setNewsDate] = React.useState("2022-06-19");
-  //   const [newsData, setNewsData] = React.useState([
-  //     {
-  //       news: " I can be a React component, multiple React components, or just sometext.",
-  //       id: 1,
-  //       date: "2022-06-19",
-  //       ischecked: false,
-  //     },
-  //   ]);
-
-  //   const [newsArray, setNewsArray] = React.useState(newsCollection);
-  //   const saveNews = () => {
-  //     let date = new Date();
-  //     let year = date.getFullYear();
-  //     let mm = date.getMonth() + 1;
-  //     let m = mm.toString();
-  //     let month = m.length === 2 ? m : "0" + m;
-  //     let dd = date.getDate();
-  //     let d = dd.toString();
-  //     let day = d.length === 2 ? d : "0" + d;
-  //     let dateString = year + "-" + month + "-" + day;
-  //     if (news === "") {
-  //       enqueueSnackbar("field was empty", {
-  //         variant: "warning",
-  //       });
-  //     } else {
-  //       let updateData = {
-  //         News: news,
-  //         News_date: dateString,
-  //         Regional_school: 1,
-  //         admin: user?.username,
-  //         method: "post",
-  //       };
-
-  //       dispatch(NewsManager(updateData));
-  //       enqueueSnackbar("News Successfully Posted", {
-  //         variant: "success",
-  //       });
-  //     }
-  //   };
-
-  //   const checkedChange = (e: any) => {
-  //     const { id, checked } = e.target;
-  //     if (id === "allselect") {
-  //       let tempArr = newsData.map((data) => {
-  //         return { ...data, ischecked: checked };
-  //       });
-  //       setNewsData(tempArr);
-  //     } else {
-  //       let tempArr = newsData.map((data) =>
-  //         data.news === id ? { ...data, ischecked: checked } : data
-  //       );
-  //       setNewsData(tempArr);
-  //     }
-  //   };
-  //   React.useEffect(() => {
-  //     if (newsDate) {
-  //       setNewsArray(newsCollection.filter((x: any) => x.News_date === newsDate));
-  //     } else {
-  //       setNewsArray(newsCollection);
-  //     }
-  //   }, [newsDate, newsCollection, dispatch]);
-
   const [localData, setLocalData] = useState(
     ctx.websiteData[props.id] === undefined
       ? newsData
       : ctx.websiteData[props.id]
   );
 
+  const[newsUpdate, setnewsUpdate] = useState();
+
+  const addCard = () => {
+    let updatedData = { 
+      para: newsUpdate,
+      id: localData.length,
+    };
+    setLocalData((prevState) => {
+      return [...prevState, updatedData];
+    }); 
+    setnewsUpdate("")
+
+
+  };
+
+  const removeCard = (value) => {
+    setLocalData((prevState) => {
+      prevState = prevState.filter((item) => item.id !== value);
+      return [...prevState];
+    });
+  };
+
+
+
   let editable = (
     <>
       <div className="container">
         <div>
-          <button type="button" class={clsx(classes.btndec, "btn btn-light")}>
+          <button onClick={addCard} type="button" class={clsx(classes.btndec, "btn btn-light")}>
             Add news
           </button>
         </div>
         <div>
-          {localData.map((data, index) => (
+          
             <>
-              <form className="mt-4">
+              <form className="mt-4" >
                 <div class="form-row">
                   <div class="col-md-12">
                     <input
@@ -155,32 +113,26 @@ export const Ticker1 = (props) => {
                       class="form-control"
                       placeholder="Put news here!!"
                       id=""
+                      value={newsUpdate}
+                      onChange={(e)=>setnewsUpdate(e.target.value)}
                     />
                   </div>
                 </div>
               </form>
             </>
-          ))}
+      
         </div>
         <div className="mt-4">
           <ul class="list-group list-group-flush">
-            <li class="list-group-item list-group-item-action">
-              Cras justo odio <RemoveCircleIcon className="float-right" />
+            {localData.map((data,index)=>(
+              <>
+              <li class="list-group-item list-group-item-action">
+              {data.para} <RemoveCircleIcon onClick={()=>removeCard(data.id)} className="float-right"  />
             </li>
-            <li class="list-group-item list-group-item-action">
-              Dapibus ac facilisis in{" "}
-              <RemoveCircleIcon className="float-right" />
-            </li>
-            <li class="list-group-item list-group-item-action">
-              Morbi leo risus <RemoveCircleIcon className="float-right" />
-            </li>
-            <li class="list-group-item list-group-item-action">
-              Porta ac consectetur ac{" "}
-              <RemoveCircleIcon className="float-right" />
-            </li>
-            <li class="list-group-item list-group-item-action">
-              Vestibulum at eros <RemoveCircleIcon className="float-right" />
-            </li>
+              </>
+            ))}
+            
+          
           </ul>
         </div>
       </div>
@@ -221,7 +173,11 @@ export const Ticker1 = (props) => {
       ) : (
         <></>
       )}
-
+      {loading && (
+        <>
+          <Loader />
+        </>
+      )}
       {ctx.isEditable ? (
         editable
       ) : (
@@ -377,7 +333,7 @@ export const Ticker1 = (props) => {
               >
                 <h6 className="text-white mb-0">
                   {" "}
-                  <AssignmentTurnedInIcon />
+                  <AssignmentTurnedInIcon className={classes.iconAd} />
                   News
                 </h6>
               </div>
@@ -389,18 +345,9 @@ export const Ticker1 = (props) => {
                   pauseOnHover={true}
                   className=" bg-white"
                 >
-                  {/* {newsCollection
-              ?.slice(Math.max(newsCollection.length - 5, 0))
-              .map((data) => (
-                <p className="mb-0 mx-1" key={data.id}>
-                  <i
-                    style={{ color: "#b500b2", fontSize: "13px" }}
-                    className="fa fa-circle my-0 mx-1 "
-                    aria-hidden="true"
-                  ></i>{" "}
-                
-                </p>
-              ))} */}
+                  {localData.map((data, index) => (<>
+                  <p key={index}>{data.para}</p>
+                  </>))}
                 </Marquee>
               </div>
             </div>
